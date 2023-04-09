5d48a04b8f9f9dbd7de0718c0f9f3e0c2f2df0c2
smaryamahmed123 <128501483+smaryamahmed123@users.noreply.github.com>
2023-04-09 14:02:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+// var jan = prompt("jan sallery");
+
+// var feb = prompt("feb sallery");
+
+// var march = prompt("march sallery");
+
+// var total = (jan + feb + march)/300000*100
+
+// // console.log(total)
+
+// if(total == 300000){
+//     console.log("excellent")
+// }
+
+// else if(total ==200000){
+//     console.log("Good")
+// }
+
+// else if(total == 100000){
+//     console.log("bad")
+// }
+
+
+
+var items = ["wire","laptope","book","head phone","potato","laptope","book","head phone",
+ "wire","laptope","book","head phone","wire","laptope","book","head phone"]
+//  console.log(items.length)
+ console.log(items)
+//  items.push("abc")
+ items.pop("zxy")
